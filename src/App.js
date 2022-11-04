9f0c02caa89d03af10c37c006f912401855f9837
gitkraken303 <johnspaxxvile@gmail.com>
2022-11-04 12:19:28 +0300

    Initial commit
    Folder structure and MUI styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import { AppBar, Container, Typography, Grow, Grid } from "@mui/material";
+
+import useStyles from "./styles";
+import memories from "./images/vLogo.png";
+import Posts from "./components/Posts/Posts";
+import Form from "./components/Form/Form";
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="lg">
+      <div
+        style={{
+          borderRadius: 15,
+          margin: "30px 0",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+          position: "static",
+          border: "1px solid #ccc",
+        }}
+        position="static"
+        color="inherit"
+      >
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memories
+        </Typography>
+        <img className={classes.image} src={memories} alt="icon" height="60" />
+      </div>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              {/* <Posts setCurrentId={setCurrentId} /> */}
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              {/* <Form currentId={currentId} setCurrentId={setCurrentId} /> */}
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
 }
 
